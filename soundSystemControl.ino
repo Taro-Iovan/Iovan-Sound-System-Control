8b78a9b8db4d0d59ae1df3f8ecfe559ad4676756
Taro-Iovan <59847576+Taro-Iovan@users.noreply.github.com>
2022-04-28 00:15:12 -0600

    Start

---
+++ soundSystemControl.ino
@@ -0,0 +1,21 @@
+//Goal: control ARGB LEDs, 2x relays for subwoofers, use IR to control receiver to automatically switch between sources
+//Author: Taro
+//
+
+
+
+
+void setup() {
+
+
+
+}
+
+
+void loop() {
+
+
+}
+
+
+
